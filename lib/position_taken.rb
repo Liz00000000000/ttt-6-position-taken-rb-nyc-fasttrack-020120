659a095468f6dede082f2540253eb4d62b53c962
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 01:27:50 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,8 @@
 require_relative '.../spec/position_taken_spec.rb'
 board = [" "," "," "," "," "," "," "," "," "]
 
-
-if move == board["X"] || movie board["O"]
+def position_taken
+if (move == board["X"] || movie board["O"])
   puts "position taken"
 else puts "not taken"
 end
