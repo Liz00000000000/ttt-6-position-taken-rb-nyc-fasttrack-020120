cefaedc9a7669dc83dd75661d61b4188bee4c787
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 01:41:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 #require_relative '.../spec/position_taken_spec.rb'
 #board = [" "," "," "," "," "," "," "," "," "]
 
-def position_taken
+def position_taken (board, index)
 if board.index == " "
    false
  else true
