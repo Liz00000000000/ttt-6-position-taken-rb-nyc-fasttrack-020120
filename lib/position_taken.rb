dc69745928793bdca2c9e0aff6cf76e08dc7ac29
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 02:49:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 #require_relative '.../spec/position_taken_spec.rb'
-#board = [" "," "," "," "," "," "," "," "," "]
-#index = ["X","O"]
+board = [" "," "," "," "," "," "," "," "," "]
+index = ["X","O"]
 
 def position_taken?(board, index)
 if index == " "
