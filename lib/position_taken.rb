419b1123a25b56c375b011b5573c36a60b331f9b
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 01:16:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+if move == board["X"] || movie board["O"]
+  puts "position taken"
+else puts "not taken"
+end
